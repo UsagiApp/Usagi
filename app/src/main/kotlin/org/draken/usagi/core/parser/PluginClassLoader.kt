3f64deb768cb44d6550bd13d443421adf10d9b9b
dragonx943 <premieregirl26@gmail.com>
2026-05-16 13:59:48 +0700

    Some fixes for integrating Tachiyomi ext into plugins, bump version

--- app/src/main/kotlin/org/draken/usagi/core/parser/PluginClassLoader.kt
+++ app/src/main/kotlin/org/draken/usagi/core/parser/PluginClassLoader.kt
@@ -20,7 +20,8 @@ class PluginClassLoader(
 			name.startsWith("org.koitharu.kotatsu.core.parser.") ||
 			name.startsWith("eu.kanade.tachiyomi.") ||
 			name.startsWith("uy.kohesive.injekt.") ||
-			name.startsWith("rx.")
+			name.startsWith("rx.") ||
+			name.startsWith("keiyoushi.")
 		) { return runCatching { findClass(name) }.getOrElse { super.loadClass(name, resolve) } }
 		return super.loadClass(name, resolve)
 	}
