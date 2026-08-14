bb760b120b2cb3d16518ebbd70169ecbf040f273
sang765 <8.0249864e+07+sang765@users.noreply.github.com>
2026-08-14 00:03:54 +0000

    fix: avoid service-owned localized resources context leak

--- app/src/release/kotlin/org/draken/usagi/core/ui/BaseService.kt
+++ app/src/release/kotlin/org/draken/usagi/core/ui/BaseService.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.LifecycleService
 
 abstract class BaseService : LifecycleService() {
 	override fun attachBaseContext(newBase: Context) {
-		super.attachBaseContext(ContextCompat.getContextForLanguage(newBase))
+		val applicationBase = newBase.applicationContext ?: newBase
+		super.attachBaseContext(ContextCompat.getContextForLanguage(applicationBase))
 	}
 }
